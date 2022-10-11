ef36895eaab840ca258c955a451e485f2ca40316
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-11 15:28:04 -0600

    Add Chicken model spec and validation tests

---
+++ spec/models/chicken_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Chicken, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :id}
+    it {should validate_numericality_of :id}
+    it {should validate_presence_of :coop_id}
+    it {should validate_numericality_of :coop_id}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :is_broody}
+    it {should validate_presence_of :clutch_count}
+    it {should validate_numericality_of :clutch_count}
+  end
+end
