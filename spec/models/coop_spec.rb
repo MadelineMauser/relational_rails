beedb297d20598cceadd8a99a53018bd092d020d
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-11 15:13:25 -0600

    Add Coop relationship test

--- spec/models/coop_spec.rb
+++ spec/models/coop_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Coop, type: :model do
-  describe "validations" do
-    it { should validate_presence_of :id }
-    it { should validate_numericality_of :id }
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :is_portable }
-    it { should validate_presence_of :nest_boxes }
-    it { should validate_numericality_of :nest_boxes }
+  describe 'validations' do
+    it {should validate_presence_of :id}
+    it {should validate_numericality_of :id}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :is_portable}
+    it {should validate_presence_of :nest_boxes}
+    it {should validate_numericality_of :nest_boxes}
+  end
+
+  describe 'relationships' do
+    xit {should have_many :coops}
   end
 end
