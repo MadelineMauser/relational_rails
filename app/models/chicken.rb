7c7f1011c07cd4cb855934d7d901f52f8b46988f
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-13 12:54:26 -0600

    Add chicken model validations

---
+++ app/models/chicken.rb
@@ -0,0 +1,8 @@
+class Chicken < ApplicationRecord
+  validates :id, presence: true, numericality: true
+  validates :coop_id, presence: true, numericality: true
+  validates :name, presence: true
+  validates :is_broody, presence: true
+  validates :clutch_count, presence: true, numericality: true
+  
+end
