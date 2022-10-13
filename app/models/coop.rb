4aebb9eb6bb8a1630a95ec84e408a87dff570576
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-13 12:52:17 -0600

    Add coop model validations

--- app/models/coop.rb
+++ app/models/coop.rb
@@ -1,3 +1,7 @@
 class Coop < ApplicationRecord
+  validates :id, presence: true, numericality: true
+  validates :name, presence: true
+  validates :is_portable, presence: true
+  validates :nest_box_num, presence: true, numericality: true
   
 end
