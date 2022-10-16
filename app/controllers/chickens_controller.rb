5c533d638120350de1fa2b1bd349b5255b0acccb
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-16 16:23:01 -0600

    Add chicken show page

--- app/controllers/chickens_controller.rb
+++ app/controllers/chickens_controller.rb
@@ -2,4 +2,8 @@ class ChickensController < ApplicationController
   def index
     @chickens = Chicken.all
   end
+
+  def show
+    @chicken = Chicken.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/coops', to: 'coops#index'
   get '/coops/:id', to: 'coops#show'
   get '/chickens', to: 'chickens#index'
+  get '/chickens/:id', to: 'chickens#show'
 end
