fef3504dd8edd164001f3511c7921aca52b7aea5
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-16 16:33:38 -0600

    Add coop chickens index test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,16 @@ Chicken.destroy_all
 Coop.destroy_all
 
 coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5)
-coop2 = Coop.create!(name: 'Cluck Hut', is_portable: false, nest_box_num: 6)
-
 chicken1 = coop1.chickens.create!(name: 'Martha', is_broody: false, clutch_count: 3)
-chicken2 = coop2.chickens.create!(name: 'Peeper', is_broody: true, clutch_count: 4)
+chicken2 = coop1.chickens.create!(name: 'Peeper', is_broody: true, clutch_count: 4)
+chicken3 = coop1.chickens.create!(name: 'Gretta', is_broody: false, clutch_count: 6)
+chicken4 = coop1.chickens.create!(name: 'Pumpkin', is_broody: true, clutch_count: 5)
+
+coop2 = Coop.create!(name: 'Cluck Hut', is_portable: false, nest_box_num: 6)
+chicken5 = coop2.chickens.create!(name: 'Big Marge', is_broody: false, clutch_count: 4)
+chicken6 = coop2.chickens.create!(name: 'Lady', is_broody: true, clutch_count: 7)
+
+
+
 
     
---
+++ spec/features/coop_chickens/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "coop chicken index page", type: :feature do
+  it 'shows each child associated with the given parent and their attributes' do
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5)
+    chicken1 = coop1.chickens.create!(name: 'Martha', is_broody: false, clutch_count: 3)
+    chicken2 = coop1.chickens.create!(name: 'Peeper', is_broody: true, clutch_count: 4)
+    chicken3 = coop1.chickens.create!(name: 'Gretta', is_broody: false, clutch_count: 6)
+
+    visit "/coops/#{coop1.id}/chickens"
+
+    expect(page).to have_content(chicken1.name)
+    expect(page).to have_content("Is broody: #{chicken1.is_broody}")
+    expect(page).to have_content("Clutch count: #{chicken1.clutch_count}")
+    expect(page).to have_content("Created at: #{chicken1.created_at}")
+    expect(page).to have_content("Updated at: #{chicken1.updated_at}")
+    expect(page).to have_content(chicken2.name)
+    expect(page).to have_content("Is broody: #{chicken2.is_broody}")
+    expect(page).to have_content("Clutch count: #{chicken2.clutch_count}")
+    expect(page).to have_content("Created at: #{chicken2.created_at}")
+    expect(page).to have_content("Updated at: #{chicken2.updated_at}")
+    expect(page).to have_content(chicken3.name)
+    expect(page).to have_content("Is broody: #{chicken3.is_broody}")
+  end
+
+end
