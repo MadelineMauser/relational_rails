bdd7f35ab5868bebc58572ee371cd154ed235519
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-17 13:15:18 -0600

    Add test to chicken index

--- spec/features/chickens/index_spec.rb
+++ spec/features/chickens/index_spec.rb
@@ -22,8 +22,27 @@ RSpec.feature "chicken index page", type: :feature do
     expect(page).to have_content("Updated at: #{chicken2.updated_at}")
   end
   it 'has a link to it that appears on every page' do
-    visit '/coops'
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5)
+    chicken1 = coop1.chickens.create!(name: 'Martha', is_broody: false, clutch_count: 3)
 
+    visit '/coops'
     click_link("Chicken Index")
+    expect(page).to have_current_path('/chickens')
+
+    visit "/coops/#{coop1.id}"
+    click_link("Chicken Index")
+    expect(page).to have_current_path('/chickens')
+
+    visit "/coops/#{coop1.id}/chickens"
+    click_link("Chicken Index")
+    expect(page).to have_current_path('/chickens')
+
+    visit "/chickens"
+    click_link("Chicken Index")
+    expect(page).to have_current_path('/chickens')
+
+    visit "/chickens/#{chicken1.id}"
+    click_link("Chicken Index")
+    expect(page).to have_current_path('/chickens')
   end
 end
