165746c6e67ec8a9557939c5f8434113271cad24
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-17 13:44:02 -0600

    Add links to coop and chicken indices

--- spec/features/coops/index_spec.rb
+++ spec/features/coops/index_spec.rb
@@ -29,4 +29,28 @@ RSpec.feature "coop index page", type: :feature do
 
     expect(page).to have_content("#{coop1.name} #{coop1.created_at}")
   end
+  it 'has a link to it that appears on every page' do
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5)
+    chicken1 = coop1.chickens.create!(name: 'Martha', is_broody: false, clutch_count: 3)
+
+    visit '/coops'
+    click_link("Coop Index")
+    expect(page).to have_current_path('/coops')
+
+    visit "/coops/#{coop1.id}"
+    click_link("Coop Index")
+    expect(page).to have_current_path('/coops')
+
+    visit "/coops/#{coop1.id}/chickens"
+    click_link("Coop Index")
+    expect(page).to have_current_path('/coops')
+
+    visit "/chickens"
+    click_link("Coop Index")
+    expect(page).to have_current_path('/coops')
+
+    visit "/chickens/#{chicken1.id}"
+    click_link("Coop Index")
+    expect(page).to have_current_path('/coops')
+  end
 end
