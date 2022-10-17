7a0e45773afdfe220411e38cd936516d35c3dbe7
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-17 10:12:01 -0600

    Add additional coop show test

--- spec/features/coops/show_spec.rb
+++ spec/features/coops/show_spec.rb
@@ -17,4 +17,15 @@ RSpec.feature "coop show page", type: :feature do
     expect(page).not_to have_content("Is portable: #{coop2.is_portable}")
     expect(page).not_to have_content("Nest Box Number: #{coop2.nest_box_num}")
   end
+  it 'shows the count of children associated with the given parent' do
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5)
+    chicken1 = coop1.chickens.create!(name: 'Martha', is_broody: false, clutch_count: 3)
+    chicken2 = coop1.chickens.create!(name: 'Peeper', is_broody: true, clutch_count: 4)
+    chicken3 = coop1.chickens.create!(name: 'Gretta', is_broody: false, clutch_count: 6)
+    chicken4 = coop1.chickens.create!(name: 'Pumpkin', is_broody: true, clutch_count: 5)
+
+    visit "/coops/#{coop1.id}"
+
+    expect(page).to have_content("Current number of chickens: #{coop1.chickens.count}")
+  end
 end
