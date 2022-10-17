83861f7ac5452fc2b79935b2694f39560d1c7cfe
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-17 09:33:20 -0600

    Add additional coop index tests

--- spec/features/coops/index_spec.rb
+++ spec/features/coops/index_spec.rb
@@ -13,9 +13,20 @@ RSpec.feature "coop index page", type: :feature do
     expect(page).to have_content(coop3.name)
   end
   it 'shows coops ordered by most recently created' do
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5, created_at: Time.now + 10.days)
+    coop2 = Coop.create!(name: 'Cluck Hut', is_portable: false, nest_box_num: 6, created_at: Time.now)
+    coop3 = Coop.create!(name: 'Roaming Home', is_portable: true, nest_box_num: 4, created_at: Time.now + 3.days)
 
+    visit '/coops'
+
+    expect("#{coop2.name}").to appear_before("#{coop3.name}", only_text: true)
+    expect("#{coop3.name}").to appear_before("#{coop1.name}", only_text: true)
   end
   it 'shows the records with their creation date next to them' do
+    coop1 = Coop.create!(name: 'Cozy Cottage', is_portable: true, nest_box_num: 5, created_at: Time.now + 10.days)
 
+    visit '/coops'
+
+    expect(page).to have_content("#{coop1.name} #{coop1.created_at}")
   end
 end
